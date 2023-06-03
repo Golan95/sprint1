ad26881b2217f4185f9bb4063a241e56a8e9a8ff
Golan95 <134696872+Golan95@users.noreply.github.com>
2023-06-03 22:00:28 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -14,17 +14,17 @@ var gCell
 
 function onInit() {
   gGame = { isOn: false, shownCount: 0, markedCount: 0, secsPassed: 0 }
-  gBoard = {minesAroundCount: 4, isShown: false, isMine: false, isMarked: true}
+  gBoard = { minesAroundCount: 0, isShown: false, isMine: false, isMarked: true }
   mainBoard = buildBoard()
   play()
   renderBoard(mainBoard)
-   
+
 }
 
 function play() {
   console.log('play');
-  startTimer()
-  runBoard(mainBoard)
+  //startTimer()
+  checkNegs(mainBoard)
 }
 
 
@@ -35,98 +35,118 @@ function buildBoard() {
   for (var i = 0; i < gSize; i++) {
     board.push([])
     for (var j = 0; j < gSize; j++) {
-      if(i === 0 && j === 0 || i=== 2 && j === 2){
-      board[i][j] = mines
-      
+      if (i === 0 && j === 0 || i === 2 && j === 2) {
+        board[i][j] = mines
+
       }
-      else{
+      else {
         board[i][j] = ' '
-        
+
       }
     }
   }
-  
-  
+
+
   console.log('board:', board)
   return board
 }
 
-function runBoard(board) {
-  var newBoard = copyMat(board)
-  for (var i = 0; i < newBoard.length; i++) {
-      for (var j = 0; j < newBoard[0].length; j++) {
-        // console.log(i, j);
-          if (newBoard[i][j] === mines) continue
-          else{
-          var numOfNegs = countNegs(i, j, board)
-          // console.log('numOfNegs',numOfNegs);
-        }
-      } 
+function checkNegs(board) {
+
+  for (var i = 0; i < board.length; i++) {
+    for (var j = 0; j < board[0].length; j++) {
+      console.log(i, j);
+      if (board[i][j] === mines) continue
+      else {
+        var numOfNegs = countNegs(i, j, board)
+        console.log('numOfNegs', numOfNegs)
+        var minesAround = minesAroundCount(i, j, board)
+        console.log('minesAround', minesAround);
+      }
+    }
   }
-  
-    return newBoard
+
+
 }
 
 function countNegs(cellI, cellJ, mat) {
   var negsCount = 0
   for (var i = cellI - 1; i <= cellI + 1; i++) {
-      if (i < 0 || i >= mat.length) continue
-      for (var j = cellJ - 1; j <= cellJ + 1; j++) {
-          if (i === cellI && j === cellJ) continue
-          if (j < 0 || j >= mat[i].length) continue
-          
-          if (mat[i][j] === mines) negsCount++
-      }
+    if (i < 0 || i >= mat.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (i === cellI && j === cellJ) continue
+      if (j < 0 || j >= mat[i].length) continue
+
+      //if (mat[i][j] !== mines) 
+      negsCount++
+    }
   }
   return negsCount
 }
 
-
+function minesAroundCount(cellI, cellJ, mat) {
+  var minesAround = 0
+  for(var i = cellI - 1; i <= cellI + 1; i++) {
+    if (i < 0 || i >= mat.length) continue
+    for (var j = cellJ - 1; j <= cellJ + 1; j++) {
+      if (i === cellI && j === cellJ) continue
+      if (j < 0 || j >= mat[i].length) continue
+      if (mat[i][j] === mines)
+      minesAround++
+    }
+  }
+  return minesAround
+}
 function renderBoard(board) {
   // console.table(board)
   var strHTML = ''
+  
   for (var i = 0; i < board.length; i++) {
-      strHTML += '<tr>'
-      for (var j = 0; j < board[0].length; j++) {
-          var currCell = board[i][j]
-          strHTML += `<td class="${currCell}"
+    strHTML += '<tr>'
+    for (var j = 0; j < board[0].length; j++) {
+      var currCell = board[i][j]
+      strHTML += `<td class="${currCell}"
                           onclick="onCellClicked(${i},${j},this)"
                           onclick="onCellMarked(${i},${j},this)"
-                          data-i="${i}" data-j="${j}"> <span class="cell">
+                          data-i="${i}" data-j="${j}"> <span class="cell" style="display: none;">
                                 ${currCell} </span>
-                      </td>`
+                      </td>`;
                       
-      }
 
-      strHTML += '</tr>'
+    }
+
+    strHTML += '</tr>'
   }
-  // console.log('strHTML:', strHTML)
+   console.log('strHTML:', strHTML)
   
   document.addEventListener('contextmenu', event => event.preventDefault());
   const elBoard = document.querySelector('.board')
   elBoard.innerHTML = strHTML
+  
+  
 }
 
 function onCellClicked(cellI, cellJ, elCell) {
   console.log('elCell:', elCell)
   
-  if(elCell.innerText === mines){
+  if (elCell.innerHTML === mines) {
+    
     console.log('game over')
     stopTimer()
     gameOver()
-    
+
   }
   return
 }
 
-function onCellMarked(cellI, cellJ, elCell){
+function onCellMarked(cellI, cellJ, elCell) {
 
 }
 
-function gameOver(){
+function gameOver() {
   console.log('Game Over')
-    gGame.isOn = false
-    
+  gGame.isOn = false
+
 }
 
 function chooseLevel(size) {
